4c8c37f917c5b19973dd9d31ebf00039ee5f8a77
bahrulajisantoso <76810040+bahrulajisantoso@users.noreply.github.com>
2024-08-28 19:11:05 +0700

    revise

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,28 +1,72 @@
 import 'dart:convert';
 
-List<UserModel> foodFromJson(String str) => List<UserModel>.from(
+List<UserModel> userFromJson(String str) => List<UserModel>.from(
     json.decode(str)['users'].map((x) => UserModel.fromJson(x)));
 
 class UserModel {
-  final String id;
-  final String firstname;
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String maidenName;
+  final int age;
+  final String gender;
+  final String email;
+  final String phone;
+  final String username;
+  final String password;
+  final String birthDate;
+  final String image;
+  final String bloodGroup;
+  final double height;
+  final double weight;
+  final String eyeColor;
 
   UserModel({
     required this.id,
-    required this.firstname,
+    required this.firstName,
+    required this.lastName,
+    required this.maidenName,
+    required this.age,
+    required this.gender,
+    required this.email,
+    required this.phone,
+    required this.username,
+    required this.password,
+    required this.birthDate,
+    required this.image,
+    required this.bloodGroup,
+    required this.height,
+    required this.weight,
+    required this.eyeColor,
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
-      id: json['id'] ?? '',
-      firstname: json['firstname'] ?? '',
+      id: json['id'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      maidenName: json['maidenName'],
+      age: json['age'],
+      gender: json['gender'],
+      email: json['email'],
+      phone: json['phone'],
+      username: json['username'],
+      password: json['password'],
+      birthDate: json['birthDate'],
+      image: json['image'],
+      bloodGroup: json['bloodGroup'],
+      height: json['height'],
+      weight: json['weight'],
+      eyeColor: json['eyeColor'],
     );
   }
+}
 
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'firstname': firstname,
-    };
+bool login(String username, String password, List<UserModel> users) {
+  for (var user in users) {
+    if (user.username == username && user.password == password) {
+      return true;
+    }
   }
+  return false;
 }
